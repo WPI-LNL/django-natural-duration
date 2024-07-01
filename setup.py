394076359a5c41a5b184a4b8c9e0b8a342da951b
techwithalext <hello@techwithalext.com>
2024-07-01 19:10:46 -0400

    update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-natural-duration',
-    version='0.1',
+    version='0.142',
     packages=['natural_duration'],
     description='A human-readable duration form field for Django',
     long_description=README,
